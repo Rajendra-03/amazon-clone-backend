21b93ce27f1b4b8b61d79565a392f7092473654c
Rajendra parida <rajendraparida4667@gmail.com>
2026-04-22 01:40:34 +0530

    Update AmazonCloneApplication.java

--- src/main/java/com/amazon/clone/AmazonCloneApplication.java
+++ src/main/java/com/amazon/clone/AmazonCloneApplication.java
@@ -2,12 +2,16 @@ package com.amazon.clone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 public class AmazonCloneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmazonCloneApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonCloneApplication.class, args);
+    }
 }
