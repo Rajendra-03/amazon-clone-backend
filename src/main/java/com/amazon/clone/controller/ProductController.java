77b2e596919234fc5abf64dfe8162c4083d6e86d
Rajendra parida <rajendraparida4667@gmail.com>
2026-04-22 03:23:06 +0530

    Comment out @RestController annotation in ProductController
    Commented out the @RestController annotation.

--- src/main/java/com/amazon/clone/controller/ProductController.java
+++ src/main/java/com/amazon/clone/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import com.amazon.clone.model.Product;
 import com.amazon.clone.repository.ProductRepository;
 
-@RestController
+//@RestController
 @RequestMapping("/products")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
